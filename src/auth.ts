bec1ca55e28516c4e17c95922855da22bff5e76a
Internev <nev@internev.com>
2024-07-09 15:40:15 +1000

    Update auth to handle edge function/prisma issues

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
+import authConfig from './auth.config'
+
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
 
@@ -7,5 +8,6 @@ const prisma = new PrismaClient()
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub],
+  session: { strategy: 'jwt' },
+  ...authConfig,
 })
